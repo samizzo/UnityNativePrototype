b0ed0363c3542c9941d5f4b43521ef4c49652c92
Sam Izzo <sam@humbug.net>
2014-05-30 16:37:22 +1000

    * Converted unity project to text mode * Added sample scene with sample code

---
+++ UnityProject/Assets/Scripts/Button.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+	void OnGUI()
+	{
+		GUIStyle style = new GUIStyle();
+		style.alignment = TextAnchor.MiddleCenter;
+		GUI.Label(new Rect(0, Screen.height * 0.1f, Screen.width, 20.0f), "This is Unity!", style);
+		if (GUI.Button(new Rect(Screen.width * 0.25f, Screen.height * 0.2f, Screen.width * 0.5f, 50), "Switch to native.."))
+		{
+			Debug.Log("Switching to native..");
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+	void Update()
+	{
+		transform.rotation = Quaternion.identity;
+		transform.Rotate(0.0f, 0.0f, Mathf.Repeat(Time.realtimeSinceStartup, 360.0f) * 100.0f);
+	}
+}

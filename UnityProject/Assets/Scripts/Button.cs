bd0a058e3594219645c9fadbe7075a485fd12e58
Sam Izzo <sam@humbug.net>
2014-06-02 00:00:53 +1000

    * Added a time counter to guy * Use timer to rotate instead of real time since startup

--- UnityProject/Assets/Scripts/Button.cs
+++ UnityProject/Assets/Scripts/Button.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class Button : MonoBehaviour
 {
+	private float m_timer = 0.0f;
+
+	void Update()
+	{
+		m_timer += Time.deltaTime;
+	}
+
 	void OnGUI()
 	{
 		GUIStyle style = new GUIStyle();
 		style.alignment = TextAnchor.MiddleCenter;
-		GUI.Label(new Rect(0, Screen.height * 0.1f, Screen.width, 20.0f), "This is Unity!", style);
+		GUI.Label(new Rect(0, Screen.height * 0.1f, Screen.width, 20.0f), "This is Unity (" + m_timer.ToString("0.00") + ")!", style);
 		if (GUI.Button(new Rect(Screen.width * 0.25f, Screen.height * 0.2f, Screen.width * 0.5f, 50), "Switch to native.."))
 		{
 			Debug.Log("Switching to native..");
--- UnityProject/Assets/Scripts/Rotate.cs
+++ UnityProject/Assets/Scripts/Rotate.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour
 {
+	private float m_timer = 0.0f;
 	void Update()
 	{
 		transform.rotation = Quaternion.identity;
-		transform.Rotate(0.0f, 0.0f, Mathf.Repeat(Time.realtimeSinceStartup, 360.0f) * 100.0f);
+		transform.Rotate(0.0f, 0.0f, Mathf.Repeat(m_timer, 360.0f) * 100.0f);
+		m_timer += Time.deltaTime;
 	}
 }

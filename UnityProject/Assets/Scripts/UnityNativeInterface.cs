2e7781e299b4238c4760b57253fe9b5e2ed25865
Sam Izzo <sam@humbug.net>
2014-06-08 18:03:35 +1000

    * Android version of SwitchToNative static method

--- UnityProject/Assets/Scripts/UnityNativeInterface.cs
+++ UnityProject/Assets/Scripts/UnityNativeInterface.cs
@@ -3,13 +3,20 @@ using System.Runtime.InteropServices;
 
 public class UnityNativeInterface
 {
-#if UNITY_IOS && !UNITY_EDITOR
-	[DllImport ("__Internal")]
-	public static extern void SwitchToNative();
-#else
+#if UNITY_EDITOR
 	public static void SwitchToNative()
 	{
-		Debug.Log("Unimplemented in non-IOS platforms");
+		Debug.Log("Only implemented on iOS and Android!");
 	}
+#elif UNITY_IOS
+	[DllImport ("__Internal")]
+	public static extern void SwitchToNative();
+#elif UNITY_ANDROID
+    public static void SwitchToNative()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        jo.Call("switchToNative");
+    }
 #endif
 }
